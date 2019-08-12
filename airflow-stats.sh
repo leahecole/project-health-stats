57e83676f0c4931b6409684e1bea4e3e14a240ab
Leah Cole <coleleah@google.com>
2019-08-12 14:36:08 -0700

    Add gitignore and bash script for stats

--- airflow-stats.sh
+++ airflow-stats.sh
@@ -3,7 +3,7 @@
 ###########
 # TWITTER #
 ###########
-
+# User must have twurl and be authorized
 # Get Twitter followers, put them in a file called twitter.json
 twurl /1.1/followers/ids.json?screen_name=ApacheAirflow >> twitter.json
 
@@ -17,11 +17,14 @@ curl -"https://pypi.org/pypi/apache-airflow/json" >> pypi.json
 ################
 # LIBRARIES.IO #
 ################
-
+#TODO: Figure out good DevX for putting in API key. RN needs to be set in command line
+#TODO: Add your API key here OR run a script to set your env vars
+#LIB_IO_API_KEY="your-key-here"
+#TODO: Add a check for variable
 # Get Libraries IO dependents, put in file libraries-io-dependents.json
-curl "https://libraries.io/api/pypi/apache-airflow/dependents?api_key=fc49aa0e27fe6aba4f932f34030629f8" >> libraries-io-dependents.json
+curl "https://libraries.io/api/pypi/apache-airflow/dependents?api_key=${LIB_IO_API_KEY}" >> libraries-io-dependents.json
 # Get Libraries IO usage, put in file libraries-io-usage.json
-curl "https://libraries.io/api/pypi/apache-airflow/usage?api_key=fc49aa0e27fe6aba4f932f34030629f8" >> libraries-io-usage.json
+curl "https://libraries.io/api/pypi/apache-airflow/usage?api_key=${LIB_IO_API_KEY}" >> libraries-io-usage.json
 
 ##########
 # GITHUB #
