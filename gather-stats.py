6dd225cbd361b95d2e6f43f05408296c46ec216c
Leah Cole <coleleah@google.com>
2019-08-14 16:45:22 -0700

    Use env vars in python script, add error checking

--- gather-stats.py
+++ gather-stats.py
@@ -1,22 +1,35 @@
 import json
+import os
 
 # twitter info
 with open('twitter.json', 'r') as read_file:
     data = json.load(read_file)
-print('@ApacheAirflow has {} followers on Twitter'.format(len(data['ids'])))
+try:
+  print('@{} has {} followers on Twitter'.format(os.environ['TWITTER_HANDLE'], len(data['ids'])))
+except KeyError:
+  print("ERROR reading number of Twitter followers. Double check that the environment variable $TWITTER_HANDLE is set and your twitter.json file is valid\n")
 
 # pypi info
 # TODO: improve what info is being printed here
 with open('pypi.json', 'r') as read_file:
   data = json.load(read_file)
-print('apache-airflow has {} releases on PyPI'.format(len(data['releases'])))
+try:
+  print('{} has {} releases on PyPI'.format(os.environ['PYPI_PACKAGE'], len(data['releases'])))
+except KeyError:
+  print("ERROR reading PyPI download info. Double check that the environment variable $PYPI_PACKAGE is set and your pypi.json file is valid\n")
 
+
+#TODO better understanding of what's being printed here
 # libraries IO info
 # Usage
 with open('libraries-io-dependents.json', 'r') as read_file:
   data = json.load(read_file)
-print('{} packages are dependent on apache-airflow'.format(len(data)))
+try:
+  print('{} packages are dependent on {}'.format(len(data), os.environ['PYPI_PACKAGE']))
+except KeyError:
+  print("ERROR reading Libraries.IO info. Double check that the environment variable $PYPI_PACKAGE is set and your libraries-io-dependents.json file is valid\n")
 
+#TODO: Better understanding of what's being printed here
 #packages dependent
 with open('libraries-io-usage.json', 'r') as read_file:
   data = json.load(read_file)
@@ -24,8 +37,12 @@ total = 0
 for key in data.keys():
   total += data[key]
 #print(data) # shows breakdown of usage by version
-print('{} OSS projects use apache-airflow'.format(total))
+try:
+  print('{} OSS projects use {}'.format(total, os.environ['PYPI_PACKAGE']))
+except KeyError:
+  print("ERROR reading Libraries.IO info. Double check that the environment variable $PYPI_PACKAGE is set and your libraries-io-usage.json file is valid\n")
 
+#TODO: better understanding of what's being printed here
 # Repos dependent
 # with open('libraries-io-dependent-repos.json', 'r') as read_file:
 #  data = json.load(read_file)
