920588ba3ea7c48f44e9f55b8708e7a863fff9bc
Leah Cole <coleleah@google.com>
2019-08-14 17:08:52 -0700

    Add Github usage stats

--- gather-stats.py
+++ gather-stats.py
@@ -48,5 +48,15 @@ except KeyError:
 #  data = json.load(read_file)
 
 # github airflow
+try:
+  with open('github-repo-info-{}.json'.format(os.environ['GITHUB_REPO']), 'r') as read_file:
+    data = json.load(read_file)
+    print('There are {} watches, {} stars, and {} forks of {}/{} on GitHub'.format(data['subscribers_count'], data['stargazers_count'], data['forks_count'], os.environ['GITHUB_ORG'], os.environ['GITHUB_REPO']))
+except KeyError:
+  print('ERROR reading GitHub info. Double check that the environment variables $GITHUB_ORG and $GITHUB_REPO are set and that your github-repo-info-${GITHUB_REPO}.json file is valid')
+except FileNotFoundError:
+  print('ERROR reading GitHub info. Double check that the environment variable \$GITHUB_REPO is set.\n')
+
+#airflow contributors
 
 # github luigi
